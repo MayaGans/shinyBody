379bd4196a08da56997f6c491629023c070e1212
Maya Gans <jaffe.maya@gmail.com>
2020-06-12 14:59:42 -0700

    adding comments

--- inst/js/shinyBody.js
+++ inst/js/shinyBody.js
@@ -1,8 +1,20 @@
+
+// in the example I use the static id
+// we need to set the idea to el
+// then use this to create the binding
+
+// $('#human-body').on('click', '.part', (ev) => {
+//  console.log({id: '#human_body', value: ev.target.id})
+//   Shiny.setInputValue('human_body', ev.target.id)
+// })
+
 $(document).on("click", ".shinyBody-btn-group > .part", function(evt) {
-
+  // stole the closest function from the group button
+  // do I need that here?
+  // could just be as simple as $(evt.target) ??
  var el = $(evt.target).closest('.part');
-
 });
+
 
 var shinyBodyBinding = new Shiny.InputBinding();
 $.extend(shinyBodyBinding, {
@@ -10,8 +22,8 @@ $.extend(shinyBodyBinding, {
     return $(scope).find(".shinyBody-btn-group[id]");
   },
   //getType: (el) => "shinyBody.buttonGroup",
-  // need to get the selected .part not sure this is the correct js here
   getValue: function getValue(el) {
+    // is .part right here? I _think_ I want to find the closest .part?
     var value = $(el).find(".part").map(function () {
       return this.value;
     }).get();
@@ -22,6 +34,9 @@ $.extend(shinyBodyBinding, {
       return null;
     }
   },
+  // not sure what is happening here for the button
+  // and how to translate that to the body part
+  // I don't have active buttons but I can add the active class
   setValue: function(el, value) {
     console.log(el.id);
     var $el = $(el);
@@ -38,11 +53,13 @@ $.extend(shinyBodyBinding, {
     }
     $el.trigger("change");
   },
+  // wtf is this
   subscribe: function(el, callback) {
     $(el).on("change.shinyBodyBinding", function(e) {
       callback();
     });
   },
+  // also this
   unsubscribe: function(el) {
     $(el).off(".shinyBodyBinding");
   },
@@ -53,4 +70,4 @@ $.extend(shinyBodyBinding, {
   }
 });
 
-Shiny.inputBindings.register(shinyBodyGroupBinding, 'shinyBody.buttonGroup');
+Shiny.inputBindings.register(shinyBodyBinding, 'shinyBody.buttonGroup');
