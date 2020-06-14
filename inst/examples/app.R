1151e1bccd88551269c4fa17f9d63e76c026aba3
Maya Gans <jaffe.maya@gmail.com>
2020-06-14 15:27:30 -0700

    adding small fixes

--- inst/examples/app.R
+++ inst/examples/app.R
@@ -2,10 +2,13 @@
 library(shiny)
 
 ui <- function() {
+
     fluidPage(
-        bodyInput("human",
-                  data = c(10,20,40,60,90,100,25,50,15,20,70,70,30),
-                  pal = "Blue"),
+        sidebarPanel(
+                bodyInput("human",
+                          data = c(10,20,40,60,90,100,25,50,15,20,70,70,30),
+                          pal = "RdPu")
+        ),
         verbatimTextOutput("debug")
     )
 }
